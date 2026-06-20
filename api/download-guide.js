850e7ccf10ed3460fa080c62a6dd80285007e087
Phatty <phatwil@gmail.com>
2026-06-19 21:30:07 -0400

    feat: add purchase date to PDF watermark

--- api/download-guide.js
+++ api/download-guide.js
@@ -121,7 +121,8 @@ module.exports = async function handler(req, res) {
     const pdfDoc  = await PDFDocument.load(arrayBuffer);
     const font    = await pdfDoc.embedFont(StandardFonts.Helvetica);
     const pages   = pdfDoc.getPages();
-    const watermarkText = `Licensed to: ${name} | ${email}`;
+    const purchaseDate  = new Date().toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
+    const watermarkText = `Licensed to: ${name} | ${email} | ${purchaseDate}`;
     const fontSize      = 11;
     const color         = rgb(0.5, 0.5, 0.5);
 
